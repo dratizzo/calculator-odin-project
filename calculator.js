e2cb92af1179bbde869ddecd5d7ba756876aa6b3
dratizzo <111133664+dratizzo@users.noreply.github.com>
2025-12-12 12:02:44 -0300

    Lógica para seleção do operador e escolha do segundo valor

--- calculator.js
+++ calculator.js
@@ -14,24 +14,93 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-// function operate(num1, operation, num2) {
-    
-// }
+function operate(actualValue, operatorSelection, newValue) {
+    switch(operatorSelection) {
+        case '+':
+            return add();
+            break;
+        case '-':
+            return subtract();
+            break;
+        case '*':
+            return multiply();
+            break;
+        case '/':
+            return divide();
+            break;
+    }
 
+    // if (operatorSelection == '+') add();
+
+};
+
+// Seleciona dígitos numéricos
 let digitButtons = document.querySelectorAll(".numbers-clear div div p");
 
-let currentValue = document.querySelector('.display-calculator p')
+// Seleciona valor do display
+let currentValue = document.querySelector('.display-calculator p');
 
-let newValue = ''
+// Seleciona operadores
+let operatorsDigitButtons = document.querySelectorAll('.operators div p')
 
+// Variável que atualiza display
+let actualValue = '';
+let newValue = '';
+
+let operatorSelection = '';
+
+// Lê cliques nos botões dos dígitos
 digitButtons.forEach((digit) => {
     digit.addEventListener('click', () => {
-        // vai concatenando o  valor
-        newValue += digit.textContent;
-        currentValue.textContent = newValue;
-        // return 
+        // Se não houver operador atualiza o primeiro número
+        if (operatorSelection == '') {
+            actualValue += digit.textContent;
+            currentValue.textContent = actualValue;
+        } else { // Se houver atualiza o segundo número
+            newValue += digit.textContent; 
+            currentValue.textContent = newValue;
+        };
     });
 });
+
+// Lê cliques nos botões de operadores
+operatorsDigitButtons.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (newValue != '') {
+            // aqui chama função para resolver a operação
+            actualValue = operate(Number(actualValue), operatorSelection, Number(newValue));
+            currentValue.textContent = actualValue;
+            operatorSelection = operator.textContent;
+        } else {
+            operatorSelection = operator.textContent;
+            console.log(operatorSelection);    
+        }
+
+    })
+});
+
+let equals = document.querySelector('.do-operation p');
+
+equals.addEventListener('click', () => {
+    // clique um um valor, faz nada
+    // clique com um valor e operador, faz operação com ele mesmo
+    // clique com dois valores e operador, faz operação normal
+});
+
+
+// seleciona operadores
+
+// clique no operator botão indica seleção do tipo da operação
+
+// clicar em novos números após clicar no operador começa segunda variável
+
+// clicar em novo operador após ter clicado em números realiza a operação
+
+// se houver um operador selecionado e você clicar em um digito coloca na segunda variável
+// após a operação reseta, a menos que tenha sido clicado em um novo operador para realizar a operação
+// resultado vai para o primeiro operando e já define o operador
+
+// -------
 
 // 1 + 3 - 5 * 8 + 1
 
